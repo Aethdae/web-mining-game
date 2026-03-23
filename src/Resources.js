7f1c150e9b99509378df86f7e3cf8751ffab9f40
Tyler Long <aethdae@gmail.com>
2026-03-23 04:51:16 -0500

    add basic styles and randomized rendering of tiles, add tiles to array (tbd better way)

--- src/Resources.js
+++ src/Resources.js
@@ -4,6 +4,7 @@ class Resources {
       tileSheet: "/assets/tilesheet.png",
     };
     this.images = {};
+    this.imageMap = {};
 
     Object.keys(this.toLoad).forEach((key) => {
       const img = new Image();
@@ -14,7 +15,16 @@ class Resources {
       };
       img.onload = () => {
         this.images[key].isLoaded = true;
-        console.log("Image loaded");
+        Promise.all([
+          createImageBitmap(img, 0, 0, 16, 16),
+          createImageBitmap(img, 0, 16, 16, 16),
+          createImageBitmap(img, 0, 32, 16, 16),
+          createImageBitmap(img, 16, 0, 16, 16),
+          createImageBitmap(img, 16, 16, 16, 16),
+          createImageBitmap(img, 16, 32, 16, 16),
+        ]).then((bmp) => {
+          this.imageMap["images"] = bmp;
+        });
       };
     });
   }
--- src/main.js
+++ src/main.js
@@ -7,14 +7,27 @@ const ctx = canvas.getContext("2d");
 const draw = () => {
   const tileSheet = resources.images.tileSheet;
   if (tileSheet.isLoaded) {
-    ctx.drawImage(tileSheet.image, 0, 0);
+    for (let x = 0; x < 320; x += 16) {
+      for (let y = 0; y < 180; y += 16) {
+        ctx.drawImage(
+          resources.imageMap.images[Math.floor(Math.random() * 3)],
+          x,
+          y,
+        );
+        if (Math.random() < 0.02) {
+          if (!(y > 175)) {
+            ctx.drawImage(resources.imageMap.images[4], x, y);
+          }
+        }
+      }
+    }
   }
 };
 function main() {
   setInterval(() => {
     console.log("hey");
     draw();
-  }, 300);
+  }, 3000);
 }
 
 main();
